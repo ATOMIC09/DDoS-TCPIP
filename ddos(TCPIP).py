60da9ed4b0dc976a27dc88288cf374f06c64e2e0
ATOMIC <66838025+ATOMIC09@users.noreply.github.com>
2021-08-22 17:26:41 +0700

    Update ddos(TCPIP).py

--- ddos(TCPIP).py
+++ ddos(TCPIP).py
@@ -1,16 +1,7 @@
 import socket
 import os
 
-banner = """         _   _             _        
-     /\  | | | |           | |       | | |
-    /  \ | |_| |_ __ _  ___| | __    | | |
-   / /\ \| __| __/ _` |/ __| |/ /    | | |
-  / ____ \ |_| || (_| | (__|   <     | | |
- /_/    \_\__|\__\__,_|\___|_|\_\    . . .
-"""
-
 class main():
-	print(banner)
 	print("===== DDoS Tool V1.1 =====\n1.This program requires Target's IPv4 and does not have a Firewall.\n2.This program sends data on the TCP/IP protocol.\n3.If you do it on one computer, it's called DoS.\n4.If you do it on multiple computers, it's called DDoS.\n5.You can cause the local game server (LAN) to crash by DDoS on the game server host.\n\n **DDoS affects your internet bandwidth.**\n\n")
 	# Def Obj
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
